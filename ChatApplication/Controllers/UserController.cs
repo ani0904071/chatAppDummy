31910bbc3fd992ff58c9551cecad953fb98d443a
Ani <anicse0904071@gmail.com>
2021-01-23 12:53:15 +0600

    custome join to retreive chats

--- ChatApplication/Controllers/UserController.cs
+++ ChatApplication/Controllers/UserController.cs
@@ -9,7 +9,9 @@ using Microsoft.EntityFrameworkCore;
 using ChatApplication.Dtos;
 using AutoMapper;
 using ChatApplication.Repositories;
-
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace ChatApplication.Controllers
 {
@@ -41,16 +43,38 @@ namespace ChatApplication.Controllers
             var textToCreate = _mapper.Map<TblChatDetails>(chatDetailsDto);
             textToCreate.CreatedAt = DateTime.Now;
             var sentText = await _chatRepo.sendText(textToCreate);
-            return StatusCode(201, new { id = sentText.ChatId, sentAt = sentText.CreatedAt,  chatMessage = sentText.ChatText, to = sentText.ToUserId });
+
+            return StatusCode(201, new { id = sentText.ChatId, sentAt = sentText.CreatedAt, chatMessage = sentText.ChatText, to = sentText.ToUserId });
         }
 
         [HttpGet]
         [Route("RerieveTexts/{fromUserID:int}/{toUserID:int}")]
         public async Task<IActionResult> RerieveChats(int fromUserID, int toUserID)
         {
-            var result = await _context.TblChatDetails.FromSqlRaw("SELECT * from TblChatDetails WHERE FromUserID = {0} AND ToUserID = {1} ", fromUserID, toUserID).ToListAsync();
-            return Ok(result);
+
+            var query = await (from ct in _context.TblChatDetails
+                         join u1 in _context.TblUser
+                         on ct.FromUserId equals u1.UserId
+                         join u2 in _context.TblUser
+                         on ct.ToUserId equals u2.UserId
+                         select new
+                         {
+                             textId = ct.ChatId,
+                             senderId = u1.UserId,
+                             sentBy = u1.FirstName,
+                             textMessage = ct.ChatText,
+                             sentAt = ct.CreatedAt,
+                             receiverId = u2.UserId,
+                             receivedBy = u2.FirstName
+                         }).Where(param => (param.senderId == fromUserID && param.receiverId == toUserID) || (param.receiverId == fromUserID && param.senderId == toUserID) ).ToListAsync();
+
+            return Ok(query);
+
         }
 
+
     }
+            
+
+
 }
